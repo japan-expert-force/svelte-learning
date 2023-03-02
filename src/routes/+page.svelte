78a0846f071d888919baf62034fc14bb354acf23
sharo-jef <sharo.jef@gmail.com>
2023-03-03 03:15:53 +0900

    init

---
+++ src/routes/+page.svelte
@@ -0,0 +1,24 @@
+<script>
+    let count = 0;
+
+    function onClick() {
+        count++;
+    }
+
+    let value = 'Hello, world!';
+</script>
+
+<h1>Home</h1>
+
+<button on:click={onClick}>クリック数: {count}</button>
+
+<div>
+    <input bind:value={value} />
+    {value}
+</div>
+
+<style lang="sass">
+    :global(body)
+        color: white
+        background: linear-gradient(to right,rgb(223,95,255),rgb(78,124,255))
+</style>
---
+++ src/routes/blog/[slug]/+page.svelte
@@ -0,0 +1,5 @@
+<script>
+    import { page } from "$app/stores";
+</script>
+
+<h1>blog: {$page.params.slug}</h1>
